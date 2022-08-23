027d7fce8927b83b4f4c8aa9c22794ae1a4f0d51
zrq <zrq5462@163.com>
2022-08-23 11:15:52 +0800

    add 提供监控api

---
+++ tcp-server/src/main/java/cn/aoe/sb/netty/server/controller/MonitorApi.java
@@ -0,0 +1,18 @@
+package cn.aoe.sb.netty.server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 提供 监控 接口
+ * @author zhaoruiqing
+ * @version 1.0
+ * @since 2022/8/23
+ */
+@Slf4j
+@RestController
+@RequestMapping("/monitor")
+public class MonitorApi {
+
+}
